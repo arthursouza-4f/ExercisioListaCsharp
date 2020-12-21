cb943ace5cd3a7c10edab9ce67aa3a41185f3824
arthursouza-4f <joaoanton384@gmail.com>
2020-12-21 12:07:43 -0300

    Adicionar arquivos de projeto.

---
+++ ExercisioList/Funcionario.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExercisioList
+{
+    class Funcionario
+    {
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double Salary { get; set; }
+
+
+
+        public void  AdicionarSalario(double porcentagem)
+        {
+            double percentual = porcentagem / 100.0; // Calcula a porcentagem para ser adicionada.
+            Salary =+ (percentual * Salary) + Salary; // Calcula quanto adicionar no salario.
+           
+
+        }
+
+
+
+
+    }
+}
---
+++ ExercisioList/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+namespace ExercisioList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            Console.WriteLine("Quantas pessoas vão ser registradas?");
+            int registro = int.Parse(Console.ReadLine());
+            List<Funcionario> funcionarios = new List<Funcionario>();
+            
+
+
+            for (int i = 0; i < registro; i++)
+            {
+                var func = new Funcionario();
+                               
+                Console.WriteLine(" Funcionario " );
+                Console.Write("ID: ");
+                func.Id = int.Parse(Console.ReadLine());
+                Console.Write("Nome: ");
+                func.Name = Console.ReadLine();
+                Console.Write("Salario: ");
+                func.Salary = double.Parse(Console.ReadLine());
+                Console.WriteLine("--------------------------");
+                funcionarios.Add(func);
+
+            }
+
+            Console.Write("Entre com id para adicionar ao salario: ");
+            int idpor = int.Parse(Console.ReadLine());
+
+            var salario = funcionarios.Find(x => x.Id == idpor);
+
+
+            if (salario == null) 
+            {
+                Console.WriteLine("Esse id não existe.");
+            }
+            else
+            {
+                Console.WriteLine("Entre com a porcentagem para ser adicionado ao salario: ");
+                double percentual = double.Parse(Console.ReadLine());
+                salario.AdicionarSalario(percentual);
+            }
+
+            Console.WriteLine("Lista Atualizada");
+            foreach (var f in funcionarios)
+            {
+                
+                
+                Console.WriteLine(f.Id + "," + f.Name + ", " + f.Salary);
+            }
+
+        }
+    }
+}
